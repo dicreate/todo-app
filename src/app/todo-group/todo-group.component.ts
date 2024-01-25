4df49c3578c2507fb8c20cc402e4516a5a17b6aa
DmitriyKovchugo <dkovchugo@mail.ru>
2024-01-25 17:21:39 +0300

    first commit

---
+++ src/app/todo-group/todo-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-group.component.html',
+  styleUrl: './todo-group.component.css'
+})
+export class TodoGroupComponent {
+
+}
